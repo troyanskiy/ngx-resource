dd80a0019b83d07b8529168898e10c30bed1c1cf
Roman Rosluk <roman.rosluk@utopix.ch>
2016-08-11 15:10:19 +0200

    Bump to v0.5.1. remove triple slash reference

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-///<reference path="./node_modules/@types/es6-shim/index.d.ts"/>
 "use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-///<reference path="./node_modules/@types/es6-shim/index.d.ts"/>
-
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/publish";
 import {Inject} from "@angular/core";
