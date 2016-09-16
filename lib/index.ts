8f429a0c66ac090659583d7876d82fc618adadba
Roman Rosluk <roman.rosluk@utopix.ch>
2016-09-16 17:12:55 +0200

    New predefined CRUD Base resource with custom keys type Bump 1.2.0

--- lib/index.ts
+++ lib/index.ts
@@ -2,6 +2,7 @@ export * from './Interfaces';
 export * from './Resource';
 export * from './ResourceAction';
 export * from './ResourceCRUD';
+export * from './ResourceCRUDBase';
 export * from './ResourceGlobalConfig';
 export * from './ResourceModule';
 export * from './ResourceParams';
