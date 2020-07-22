594596c2ff2e5ce6d70da5ae55fb5fbfa3c40a7f
Adrian Ber <beradrian@yahoo.com>
2020-07-22 13:41:46 +0300

    customize id property in inherited classes too

--- projects/ngx-resource/core/src/lib/ResourceCommon/ResourceCRUDObservable.ts
+++ projects/ngx-resource/core/src/lib/ResourceCommon/ResourceCRUDObservable.ts
@@ -3,9 +3,9 @@ import { ResourceCRUDBase } from './ResourceCRUDBase';
 import { Observable } from 'rxjs';
 
 
-export abstract class ResourceCRUDObservable<TQuery, TShort, TFull, TQueryResult = TShort[]>
+export abstract class ResourceCRUDObservable<TQuery, TShort, TFull, TQueryResult = TShort[], TId = any, TReference = {id: TId}>
   extends ResourceCRUDBase<TQuery, TShort, TFull, TQueryResult,
-    Observable<TQueryResult>, Observable<TFull>, Observable<any>> {
+    Observable<TQueryResult>, Observable<TFull>, Observable<any>, TId, TReference> {
 
   protected readonly $crudReturnAs = ResourceActionReturnType.Observable;
 
